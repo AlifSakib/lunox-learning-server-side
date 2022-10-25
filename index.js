0c4dc7fbe93d8504b9205a96ad2ab814a160e05b
AlifSakib <alifsakib@gmail.com>
2022-10-25 06:45:36 +0600

    added categories data

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+const courses = require("./data/courses.json");
+const categories = require("./data/categories.json");
+
 app.get("/", (req, res) => {
   res.send("Lunox is Working. . .");
 });
+
+app.get;
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port} `);
